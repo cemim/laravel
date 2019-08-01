b4ecdb3cc103e5f95b70193928f35d81882543d6
Filipe Ferreira <filipe.ferreira@tri.com.br>
2019-08-01 09:18:02 -0300

    Implementação do método show dos clientes

--- 02_controladores/app/Http/Controllers/ClienteControlador.php
+++ 02_controladores/app/Http/Controllers/ClienteControlador.php
@@ -80,7 +80,9 @@ class ClienteControlador extends Controller
 	 */
 	public function show($id)
 	{
-		//
+		$clientes = session('clientes');
+        $cliente = $clientes[$id - 1];
+        return view('clientes.info', compact(['cliente']));
 	}
 
 	/**
--- 02_controladores/resources/views/clientes/index.blade.php
+++ 02_controladores/resources/views/clientes/index.blade.php
@@ -11,6 +11,7 @@
 		<li>
 			{{ $c['nome'] }} |
 			<a href="{{ route('clientes.edit', $c['id']) }}">Editar</a>
+			<a href="{{ route('clientes.show', $c['id']) }}">Info</a>
 		</li>
 		@endforeach
 	</ol>
