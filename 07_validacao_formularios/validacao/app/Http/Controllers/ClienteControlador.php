7e612b789cc8496f759dad3d23760e3e9b3490e3
Filipe Ferreira <filipe.ferreira@tri.com.br>
2019-08-06 17:49:47 -0300

    Validação de dados

---
+++ 07_validacao_formularios/validacao/app/Http/Controllers/ClienteControlador.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cliente;
+
+class ClienteControlador extends Controller
+{
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$clientes = Cliente::all();
+		return view('clientes.cliente', compact('clientes'));
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		return view('clientes.novocliente');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		// Validar dados obrigatórios
+		// Minimo e máximo de caracteres
+		// Campo nome único na tabela clientes
+		// Validar campo email
+		$request->validate([
+			'nome'		=> 'required|min:3|max:20|unique:clientes',
+			'idade'		=> 'required',
+			'endereco'	=> 'required',
+			'email'		=> 'required|email'
+		]);
+
+		$cliente = new Cliente();
+		$cliente->nome = $request->input('nome');
+		$cliente->idade = $request->input('idade');
+		$cliente->endereco = $request->input('endereco');
+		$cliente->email = $request->input('email');
+		$cliente->save();
+		return redirect('/');
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+}
---
+++ 07_validacao_formularios/validacao/resources/views/clientes/novocliente.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastro de Cliente</title>
+	<link rel="stylesheet" href="{{asset('css/app.css')}}">
+	<meta name="csrf-token" content="{{csrf_token()}}">
+</head>
+<body>
+	<main role="main">
+		<div class="row">
+			<div class="container col-md-8 offset-md-2">
+				<div class="card border">
+					<div class="card-header">
+						<div class="card-title">
+							Cadastro de Cliente
+						</div>
+					</div>
+					<div class="card-body">
+						<form action="/cliente" method="post">
+							@csrf
+							<div class="form-group">
+								<label for="idNome">Nome do Cliente</label>
+								<input type="text" class="form-control" id="idNome" name="nome" placeholder="Nome do Cliente">
+							</div>
+							<div class="form-group">
+								<label for="idIdade">Idade</label>
+								<input min="16" max="150" type="number" class="form-control" id="idIdade" name="idade" placeholder="Idade do Cliente">
+							</div>
+							<div class="form-group">
+								<label for="idEndereco">Endereço</label>
+								<input type="text" class="form-control" id="idEndereco" name="endereco" placeholder="Endereço do Cliente">
+							</div>
+							<div class="form-group">
+								<label for="idEmail">E-mail</label>
+								<input type="email" class="form-control" id="idEmail" name="email" placeholder="E-mail do Cliente">
+							</div>
+							<button type="submit" class="btn btn-primary btn sm">Salvar</button>
+							<button type="cancel" class="btn btn-danger btn sm">Cancelar</button>
+						</form>
+					</div>
+					<!-- Validação Laravel -->
+					@if($errors->any())
+						<div class="card-footer">
+							@foreach($errors->all() as $error)
+								<div class="alert alert-danger" role="alert">
+									{{$error}}
+								</div>
+							@endforeach
+						</div>						
+					@endif
+				</div>
+			</div>
+		</div>
+	</main>
+	<script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+</body>
+</html>
